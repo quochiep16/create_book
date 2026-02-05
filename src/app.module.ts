a6650b727758ea9caeebe33f44f97eb5cc0e0d9f
quochiep16 <quochiep1610@gmail.com>
2026-02-05 23:33:10 +0700

    liên kêt data base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // Load .env
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // MySQL config từ .env
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DB_HOST'),
+        port: Number(config.get('DB_PORT')),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // dev only
+        timezone: '+07:00',
+      }),
+    }),
+
+    BookModule,
+  ],
 })
 export class AppModule {}
